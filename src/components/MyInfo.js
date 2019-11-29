c3cb74e8f56fc87982348d1e74cb5a4452b90fb7
SD Peralta <sdperalta@SDs-MacBook-Pro.local>
2019-11-29 13:09:38 -0600

    Changing aboutme description and intro size

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -5,11 +5,11 @@ const AboutMe = () => {
     return (
         <section className="myInfoContainer">
             <p className="aboutTitle">ABOUT ME</p>
-            <p className="aboutDescrip">{`I’m a woman passionate about dancing and music, I enjoy to travel and be amazed 
-            with the different landscapes, flavors and traditions of the places I go. I see myself  as an innate dreamer 
-            who always seeks to translate ideas into concrete actions, curious to explore and learn new things.
+            <p className="aboutDescrip">{`I’m a woman passionate about dancing, music and travel. I'm curious to search, explore 
+            and learn new things. I see myself as a decisive person who always seeks to translate ideas into concrete actions, 
+            I resort to empathy to understand people needs and I rely on congruence to align what I think with what I say and do.
             I studied the bachelor in Business Administration with experience in the Financial area but now I’m 
-            discovering a big interest in graphic design, programming and technology ❤❤`}
+            discovering a big interest in graphic design, programming and technology.`}
             <br></br><br></br>
             {`I have the goal to get involve in social activities aimed to fight for the well-being and development 
             of the communities and ecosystems around me, so I love the idea of ​​being able to create innovative web products and 
--- src/components/MyInfoSpanish.js
+++ src/components/MyInfoSpanish.js
@@ -5,12 +5,11 @@ const AboutMeSpanish = () => {
     return (
         <section className="myInfoContainer">
             <p className="aboutTitle">ACERCA DE MI</p>
-            <p className="aboutDescrip"> {`Soy una mujer apasionada con el baile y la música, disfruto mucho 
-            viajar e impresionarme con los diversos paisajes, sabores y tradiciones de los lugares que visito. 
-            Me considero una soñadora innata que busca siempre plasmar las ideas en acciones concretas, curiosa
-            por explorar y aprender. 
-            Estudié la licenciatura en Administración de Empresas con experiencia en el área de Finanzas
-            pero actualmente estoy descubriendo un gran interés por el diseño gráfico, la programación y la tecnología ❤❤`}
+            <p className="aboutDescrip"> {`Soy una mujer apasionada con el baile, la música y los viajes. Curiosa por investigar, 
+            explorar y aprender cosas nuevas. Me considero una persona determinante que siempre busca trasladar las ideas en acciones
+             concretas, recurro a la empatía para entender las necesidades de las pesonas y me apoyo en la congruencia para alinear lo que pienso con lo 
+            que digo y hago. Estudié la licenciatura en Administración de Empresas con experiencia en el área de Finanzas
+            pero actualmente estoy descubriendo un gran interés por el diseño gráfico, la programación y la tecnología.`}
              <br></br><br></br>
             {`Tengo el objetivo de participar en actividades 
             orientadas a luchar por el bienestar y el desarrollo de las comunidades y ecosistemas que me rodean, así que me encanta la idea de poder crear productos web 
