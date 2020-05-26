bc04ee8f0fb98f22602b4423d3593e5dc4301c54
Denisse Peralta <denisse.peralta@astrazeneca.com>
2020-05-25 20:51:45 -0500

    Changing background and icons

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,16 +7,16 @@ const ContactMe = () => {
         <section className="contactMeContainer">
             <section className="contactMeList">
                 <a href="https://www.linkedin.com/in/denisse-peralta-barrales/" target="_blank" rel="noopener noreferrer">
-                    <img src={require('../images/linkedin.png')} alt="Social Media" className="contactIcon"></img>
+                    <img src={require('../images/linkedin.svg')} alt="Social Media" className="contactIcon"></img>
                 </a>
                 <a href="https://github.com/DenissePeralta" target="_blank" rel="noopener noreferrer">
-                    <img src={require('../images/github.png')} alt="Social Media" className="contactIcon"></img>
+                    <img src={require('../images/github.svg')} alt="Social Media" className="contactIcon"></img>
                 </a>
                 <a href="mailto:dnisse-x00@hotmail.com?Subject=He%20revisado%20tu%20portafolio%20/%20I've%20seen%20your%20portfolio" target="_blank" rel="noopener noreferrer">
-                    <img src={require('../images/email.png')} alt="Social Media" className="contactIcon"></img>
+                    <img src={require('../images/email.svg')} alt="Social Media" className="contactIcon"></img>
                 </a>
                 <a href={CV} target="_blank" rel="noopener noreferrer">
-                    <img src={require('../images/resume.png')} alt="Social Media" className="contactIcon"></img>
+                    <img src={require('../images/resume.svg')} alt="Social Media" className="contactIcon"></img>
                 </a>
             </section>
             <p className="contactMeTitle">CONTACT ME</p>
--- src/components/MySkillsSpanish.js
+++ src/components/MySkillsSpanish.js
@@ -5,25 +5,23 @@ const MySkillsSpanish = () => {
     return (
         <section className="mySkillsContainer">
             <section className="mySkillsList">
-             <img src={require('../images/tech1.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech2.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech3.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech4.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech5.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech6.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech7.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech8.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech9.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/tech10.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor1.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor2.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor3.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor4.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor5.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor6.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor7.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor8.png')} alt="My Skills" className="skillsIcons"></img>
-             <img src={require('../images/valor9.png')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech1.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech2.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech3.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech4.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech5.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech6.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech7.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech8.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech9.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/tech10.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor1.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor2.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor3.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor4.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor5.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor6.svg')} alt="My Skills" className="skillsIcons"></img>
+             <img src={require('../images/valor7.svg')} alt="My Skills" className="skillsIcons"></img>
             </section>
             <p className="skillsTitle">HABILI DADES</p>
         </section>
