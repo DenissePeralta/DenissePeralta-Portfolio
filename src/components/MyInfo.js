ee74f86810bf17f631b71506e7371f1b17bdba83
Denisse Peralta <denisse.peralta@astrazeneca.com>
2020-05-27 21:05:47 -0500

    Adding some media queries

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -6,14 +6,13 @@ const AboutMe = () => {
         <section className="myInfoContainer">
             <p className="aboutTitle">ABOUT ME</p>
             <p className="aboutDescrip">{`I have a real passion about dancing, music and travel but I also enjoy exploring into the life mysteries,  
-            learning new things and master some other abilities. I always seeks to translate the ideas into concrete actions and on my way to that goal,
+            learning new things and master some other abilities. I always seeks to translate the ideas into concrete actions, and on my way to that goal,
             I implement different ethical values that defines who I am and the essence that I print in every action or decision.`}
             <br></br><br></br>
-            {`I studied the bachelor in Business Administration with experience in the Financial area, but now, I’m 
-            discovering a big interest in the technology field as AI, IoT, UX/UI, graphic design and software development.`}
-            <br></br><br></br>
-            {`I love the fact that I can use my skills to create innovative web products and 
-            incredible applications to provide digital solutions to some of the challenges lived day by day in the society, community and even in the world.`}</p>
+            {`I studied the bachelor in Business Administration with experience in the Financial area but now, I’m 
+            discovering a big interest in the technology field such as Artificial Iintelligence, IoT, UX/UI, graphic design and software development. I love the fact 
+            that I can use my creativity and skills to create innovative web products and incredible applications to provide digital solutions to some 
+            of the challenges lived day by day in the society, community and even in the world.`}</p>
         </section>
     );
   }
--- src/components/MyInfoSpanish.js
+++ src/components/MyInfoSpanish.js
@@ -9,11 +9,10 @@ const AboutMeSpanish = () => {
             de los misterios de la vida, aprender cosas nuevas y dominar algunas otras habilidades. Siempre busco trasladar las ideas en acciones concretas 
             y en mi camino hacia ese objetivo, implemento diferentes valores éticos que definen quién soy y la esencia que imprimo en cada acción o decisión.`}
              <br></br><br></br>
-            {`Estudié la licenciatura en Administración de Empresas con experiencia en el área de finanzas, pero ahora, estoy descubriendo un gran interés en el 
-            campo de la tecnología como IA, IoT, UX/UI, diseño gráfico y desarrollo de software. `}
-             <br></br><br></br>
-            {`Me encanta el hecho de que puedo usar mis habilidades para crear productos web innovadores y aplicaciones increíbles, con el fin de proporcionar 
-            soluciones digitales a algunos de los retos y desafíos que se viven día a día en la sociedad, la comunidad e incluso en el mundo. `}</p>
+            {`Estudié la licenciatura en Administración de Empresas con experiencia en el área de finanzas pero ahora, estoy descubriendo un gran interés en el 
+            campo de la tecnología como la Inteligencia Artificial, IoT, UX/UI, diseño gráfico y desarrollo de software. Me encanta el hecho de que puedo 
+            usar mi creatividad para crear productos web innovadores y aplicaciones increíbles, con el fin de proporcionar 
+            soluciones digitales a algunos de los retos y desafíos que se viven día a día en la sociedad, en la comunidad e incluso en el mundo. `}</p>
         </section>
     );
   }
