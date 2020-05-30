3465d95fc95ecb74e2d9314c18a0acc8dd9da17e
Denisse Peralta <denisse.peralta@astrazeneca.com>
2020-05-30 15:24:21 -0500

    Editing the aboutme Description

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -5,14 +5,14 @@ const MyInfo = () => {
     return (
         <section className="myInfoContainer">
             <p className="aboutTitle">ABOUT ME</p>
-            <p className="aboutDescrip">{`I have a real passion about dancing, music and travel but I also enjoy exploring into the life mysteries,  
-            learning new things and master some other abilities. I always seeks to translate the ideas into concrete actions, and on my way to that goal,
-            I implement different ethical values that defines who I am and the essence that I print in every action or decision.`}
+            <p className="aboutDescrip">{`I have a real passion about dancing, music and travel, but I also enjoy exploring into the life mysteries, learn and gain 
+            knowledge in new topics and master some other abilities. I always seeks to translate the ideas into concrete actions, and on my way to that goal,
+            I implement different values that defines who I am and the essence that I print in every interaction or decision that I make.`}
             <br></br><br></br>
-            {`I studied the bachelor in Business Administration with experience in the Financial area but now, I’m 
-            discovering a big interest in the technology field such as Artificial Iintelligence, IoT, UX/UI, graphic design and software development. I love the fact 
-            that I can use my creativity and skills to create innovative web products and incredible applications to provide digital solutions to some 
-            of the challenges lived day by day in the society, community and even in the world.`}</p>
+            {`I studied the bachelor in Business Administration with experience in the Financial area, but now, I’m discovering a big interest in the technology 
+            field such as Artificial Iintelligence, IoT, UX/UI, graphic design and software development. I love the fact 
+            that I can use my creativity and skills to collaborate in the creation of innovative web products and incredible applications to provide digital solutions 
+            to some of the challenges lived day by day in the community and even in the world.`}</p>
         </section>
     );
   }
--- src/components/MyInfoSpanish.js
+++ src/components/MyInfoSpanish.js
@@ -5,14 +5,14 @@ const MyInfoSpanish = () => {
     return (
         <section className="myInfoContainer">
             <p className="aboutTitle">ACERCA DE MI</p>
-            <p className="aboutDescrip"> {`Tengo una verdadera pasión por el baile, la música y los viajes, pero también disfruto de explorar dentro 
-            de los misterios de la vida, aprender cosas nuevas y dominar algunas otras habilidades. Siempre busco trasladar las ideas en acciones concretas 
-            y en mi camino hacia ese objetivo, implemento diferentes valores éticos que definen quién soy y la esencia que imprimo en cada acción o decisión.`}
+            <p className="aboutDescrip"> {`Tengo una verdadera pasión por el baile, la música y los viajes, pero también disfruto de explorar dentro de los misterios 
+            de la vida, aprender y obtener conocimientos en nuevos temas así como dominar algunas otras habilidades. Siempre busco trasladar las ideas en acciones 
+            concretas y en mi camino hacia ese objetivo, implemento diferentes valores que definen quién soy y la esencia que imprimo en cada interacción o decisión que tomo.`}
              <br></br><br></br>
-            {`Estudié la licenciatura en Administración de Empresas con experiencia en el área de finanzas pero ahora, estoy descubriendo un gran interés en el 
-            campo de la tecnología como la Inteligencia Artificial, IoT, UX/UI, diseño gráfico y desarrollo de software. Me encanta el hecho de que puedo 
-            usar mi creatividad para crear productos web innovadores y aplicaciones increíbles, con el fin de proporcionar 
-            soluciones digitales a algunos de los retos y desafíos que se viven día a día en la sociedad, en la comunidad e incluso en el mundo. `}</p>
+            {`Estudié la licenciatura en Administración de Empresas con experiencia en el área de finanzas, pero ahora, estoy descubriendo un gran interés en el 
+            campo de la tecnología como lo es la Inteligencia Artificial, nternet de las cosas, experiencia de usuario, diseño gráfico y desarrollo de software. Me encanta el 
+            hecho de que puedo usar mi creatividad y destreza para colaborar en la creación de productos web innovadores y aplicaciones increíbles, con el fin de 
+            proporcionar soluciones digitales a algunos de los retos y desafíos que se viven día a día en la comunidad e incluso en el mundo.`}</p>
         </section>
     );
   }
