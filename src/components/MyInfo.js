4610e10008850196dd43f30e1e478438dedc337d
Denisse Peralta <denisse.peralta@astrazeneca.com>
2021-04-15 11:39:04 -0500

    Updating About me text

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -5,13 +5,14 @@ const MyInfo = () => {
     return (
         <section className="myInfoContainer">
             <p className="aboutTitle">ABOUT ME</p>
-            <p className="aboutDescrip">{`I have a real passion about dancing, music and travel, but I also enjoy exploring into the life mysteries, gain 
-            knowledge in new topics and master other abilities. I always seek to translate the ideas into concrete actions, and on my way to that goal,
-            I implement different core values that define who I am and the essence that I print in each decision that I make.`}
+            <p className="aboutDescrip">{`I have a real passion about dancing, music, Sci-Fi movies/books and travel but I also enjoy reinventing myself, exploring into different topics, 
+            conquering new goals and mastering other abilities. I always seek to translate the ideas into actions with the support of core values that define who I am and the way 
+            I interact and respect others.`}
             <br></br><br></br>
-            {`I studied the bachelor in Business Administration with experience in the Financial area, but now, I’m discovering a big interest in the technology 
-            field such as AI, IoT, UX/UI, graphic design and software development. I love the fact that I can use my creativity and skills to 
-            collaborate in the creation of innovative web products and incredible applications to provide digital solutions 
+            {`I studied the bachelor in Business Administration with experience in Finance, but recently I’ve switched my professional path to start a new career in the technology field. 
+            I'm discovering amazing areas such as AI, IoT, UX/UI, graphic design, web development and software engineer in which I would like to leave a positive impact with my work and perspective.`}
+            <br></br><br></br>
+            {`I love the fact that I can use my creativity and skills to collaborate in the creation of innovative web products and incredible applications to provide digital solutions 
             to some of the challenges lived day by day in the society.`}</p>
         </section>
     );
--- src/components/MyInfoSpanish.js
+++ src/components/MyInfoSpanish.js
@@ -5,16 +5,19 @@ const MyInfoSpanish = () => {
     return (
         <section className="myInfoContainer">
             <p className="aboutTitle">ACERCA DE MI</p>
-            <p className="aboutDescrip"> {`Tengo una verdadera pasión por el baile, la música y los viajes, pero también disfruto explorar dentro de los misterios 
-            de la vida, obtener conocimientos nuevos así como dominar y ejercitar otras habilidades. Mi objetivo principal es trasladar las ideas en acciones 
-            concretas y en mi camino hacia ese objetivo, voy construyendo las bases con ayuda de diferentes valores que definen quién soy y la esencia que imprimo en cada decisión que tomo.`}
+            <p className="aboutDescrip"> {`Tengo una verdadera pasión por el baile, la música, los libros/películas de Ciencia Ficción y los viajes, pero también disfruto reinventándome, 
+            explorando diferentes temas, conquistando nuevos desafíos así como dominando otras habilidades. Siempre busco trasladar las ideas en acciones concretas con la ayuda de diferentes 
+            valores que definen quién soy y la forma en que interactúo y respeto a los demás.`}
              <br></br><br></br>
-            {`Estudié la licenciatura en Administración de Empresas con experiencia en el área de finanzas, pero ahora estoy descubriendo un gran interés en el 
-            campo de la tecnología como lo es la inteligencia artificial, el internet de las cosas, UX/UI, diseño gráfico y desarrollo de software. Me encanta el 
-            hecho de que puedo usar mi creatividad y destreza para colaborar en la creación de productos web innovadores y aplicaciones increíbles con el fin de 
-            proporcionar soluciones digitales a diversos desafíos que se viven día a día en la sociedad.`}</p>
+            {`Estudié la licenciatura en Administración de Empresas con experiencia en Finanzas, pero recientemente he cambiado mi trayectoria profesional para comenzar una nueva carrera 
+            en el mundo de la tecnología. Estoy descubriendo áreas magníficas como lo es la inteligencia artificial, el internet de las cosas, UX/UI, diseño gráfico, desarrollo web e ingeniería de 
+            software, en las cuáles me gustaría dejar un impacto positivo con mi perspectiva y trabajo.`}
+            <br></br><br></br>
+            {`Me encanta el hecho de que puedo usar mi creatividad y destreza para colaborar en la creación de productos web innovadores y aplicaciones increíbles, con el fin de proporcionar 
+            soluciones digitales a diversos desafíos que se viven día a día en la sociedad.`}</p>
         </section>
     );
   }
   
   export default MyInfoSpanish;
+   
